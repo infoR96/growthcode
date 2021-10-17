d02d405e7303cf36afbfebc2c7b1c797c473f91f
giancarlo <giangc196@gmail.com>
2021-10-17 01:25:08 -0500

    test

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
-const { dbConnection } = require('./database/config');
+
 
 // Crear el servidor de express
 const app = express();
 
 // Base de datos
-dbConnection();
+
 
 // CORS
 app.use(cors())
@@ -18,12 +18,10 @@ app.use( express.static('public') );
 // Lectura y parseo del body
 app.use( express.json() );
 
-// Rutas
-app.use('/api/auth', require('./routes/auth') );
 
-
-
-
+app.get('/work',(req,res)=>{
+    res.redirect('./')
+})
 // Escuchar peticiones
 app.listen( process.env.PORT, () => {
     console.log(`Servidor corriendo en puerto ${ process.env.PORT }`);
